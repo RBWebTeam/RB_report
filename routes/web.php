30537dc80ba4a31c66cc9f50bef9b61a68aabe58
Avinash Pole <avipole1994@gmail.com>
2018-02-08 11:08:30 +0530

    deleted unnecessary file

--- routes/web.php
+++ routes/web.php
@@ -15,23 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('PiechartProductwise','PiechartController@getpiechartnew2');
-// Route::get('PiechartBankwise','PiechartController@getpiechartnew3');
-// Route::get('PiechartStatus','PiechartController@getpiechartnew4');
+
 Route::get('PiechartLeadStatus','PiechartController@getpiechartnew5');
-//Route::get('PiechartLeadDetails','PiechartController@getpiechartnew6');
-
-
-//Route::get('PiechartCityWise','PiechartController@PiechartCitywise');
-//Route::get('BarChartdemo','PiechartController@BarchartDemo1');
-
-//Route::post('BarChartdemo','PiechartController@BarchartDemo1');
-
-// Route::post('/getleaddetails/{id}','PiechartController@loadleadstatus');
-//Route::get('Piechartteamwise','PiechartController@piechatteamwise');
-//Route::get('welcome','PiechartController@Piechartwelcome');
-//Route::get('PiechartCombo','PiechartController@Piechartcombo1');
-
 
 
 
